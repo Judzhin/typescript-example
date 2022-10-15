b33d4aa4b35d7248f0a3abe2d28902722e40d8ed
Judzhin Miles <info@msbios.com>
2022-10-15 21:57:40 +0300

    Add File

--- 01.types.js
+++ 01.types.js
@@ -1,4 +1,8 @@
-var isString = "Hello World!";
-console.log(isString);
 var isTrue = true;
 var isFalse = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var str = 'Hello TypeScript';
+var nums01 = [1, 2, 3];
+var nums02 = [1, 2, 3]; // generic array types
--- 01.types.ts
+++ 01.types.ts
@@ -1,5 +1,27 @@
-const isString: string = "Hello World!"
-console.log(isString);
+const isTrue:boolean = true
+const isFalse:boolean = false
 
-const isTrue: boolean = true
-const isFalse: boolean = false
+const int:number = 42
+const float:number = 4.2
+const num:number = 3e10
+
+const str:string = 'Hello TypeScript'
+
+const nums01:number[] = [1, 2, 3]
+const nums02:Array<number> = [1, 2, 3] // generic array types
+
+const words:string[] = ['Hello', 'TypeScript']
+
+// Tuple 
+const contact:[string, number] = ['Phone', 0987654321]
+
+// Any
+// let variable = 42
+let variable: any = 42
+// variable = 'contact';
+variable = 'contact';
+variable = [];
+
+function sayHello(name:string): void {
+    console.log(`Hello ${name}`);
+}
