00ab6f15d61e366e4ae8c79a5fd7fd56788b75f5
Judzhin Miles <info@msbios.com>
2022-10-16 12:27:14 +0300

    Example Interface

--- 01.types.js
+++ 01.types.js
@@ -3,6 +3,29 @@ var isFalse = false;
 var int = 42;
 var float = 4.2;
 var num = 3e10;
-var str = 'Hello TypeScript';
+var str = "Hello TypeScript";
 var nums01 = [1, 2, 3];
 var nums02 = [1, 2, 3]; // generic array types
+var words = ["Hello", "TypeScript"];
+// Tuple
+var contact = ["Phone", 0987654321];
+// Any
+// let variable = 42
+var variable = 42;
+// variable = 'contact';
+variable = "New String";
+variable = [];
+function sayMyName(name) {
+    console.log("Hello ".concat(name));
+}
+sayMyName("John");
+// Never called
+function throwError(message) {
+    throw new Error(message);
+}
+function infinity() {
+    while (true) { }
+}
+var login = "admin";
+var id1 = "UUID";
+var id2 = 123;
--- 01.types.ts
+++ 01.types.ts
@@ -1,27 +1,51 @@
-const isTrue:boolean = true
-const isFalse:boolean = false
+const isTrue: boolean = true;
+const isFalse: boolean = false;
 
-const int:number = 42
-const float:number = 4.2
-const num:number = 3e10
+const int: number = 42;
+const float: number = 4.2;
+const num: number = 3e10;
 
-const str:string = 'Hello TypeScript'
+const str: string = "Hello TypeScript";
 
-const nums01:number[] = [1, 2, 3]
-const nums02:Array<number> = [1, 2, 3] // generic array types
+const nums01: number[] = [1, 2, 3];
+const nums02: Array<number> = [1, 2, 3]; // generic array types
 
-const words:string[] = ['Hello', 'TypeScript']
+const words: string[] = ["Hello", "TypeScript"];
 
-// Tuple 
-const contact:[string, number] = ['Phone', 0987654321]
+// Tuple
+const contact: [string, number] = ["Phone", 0987654321];
 
 // Any
 // let variable = 42
-let variable: any = 42
+let variable: any = 42;
 // variable = 'contact';
-variable = 'contact';
+variable = "New String";
 variable = [];
 
-function sayHello(name:string): void {
-    console.log(`Hello ${name}`);
+function sayMyName(name: string): void {
+  console.log(`Hello ${name}`);
 }
+
+sayMyName("John");
+
+// Never called
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+function infinity(): never {
+  while (true) {}
+}
+
+// Type
+type Login = string;
+
+const login: Login = "admin";
+// const registration: Login = 2;
+
+type ID = string | number
+const id1 : ID = "UUID";
+const id2 : ID = 123;
+// const id3 : ID = true;
+
+type SomeType = string | null | undefined;
